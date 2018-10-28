b0e072806973b3f43c34941556254cd7833040e6
Carlos Martinez <mrcaleche@gmail.com>
2018-10-28 15:52:56 -0600

    Fixed the issue of the users

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+user=$1
+
 log_msg() {
   msg=$1
   # green messages, because green is love
@@ -8,48 +10,44 @@ log_msg() {
 
 install_utils() {
   log_msg "installing zsh and a nice theme"
-  dnf install -y zsh
+  sudo dnf install -y zsh
 
-  # ZSH!
-  omzsh=~/Documents/bzsh
-  mkdir $omzsh
-
-  pushd $omzsh
+  su - $user
   # now install the oh-my-zsh with all the beautiful teams :D
-  sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-  popd -1
-  # setup the theme that I like
-  cat 'ZSH_THEME="avit"' >> ~/.zshrc
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+  # setup the theme that I like
   log_msg "Configuring VIM"
   touch ~/.vimrc
+  ls -lh ~/.vimrc
   cat vim_configuration.vim >> ~/.vimrc
 }
 
 log_msg "installing git"
-dnf install -y git
+sudo dnf install -y git
 
 log_msg "installing node dependencies"
 # node <3
-dnf install -y npm node
-npm -g install n
-n latest
+sudo dnf install -y npm node
+sudo npm -g install n
+sudo n latest
 
 
 # java stuff
 j_version="1.8.0"
 log_msg "installing Java, version $j_version"
 # hopefully will be the same name on the new distribution xD
-dnf install -y "java-$j_version-openjdk"
+sudo dnf install -y "java-$j_version-openjdk"
 
 log_msg "installing build tools for Java"
 # java build tools
-dnf install -y ant maven
+sudo dnf install -y ant maven
 
 # relational database
 log_msg "Installing mariadb"
-dnf install -y mariadb mariadb-server
+sudo dnf install -y mariadb mariadb-server
 
 install_utils
 
 log_msg "script finished, review log to make sure everything got properly installed"
+
